c6e81cf4b8aa21425815a1e80b24604e68b83723
Brittany Price <bprice0496@gmail.com>
2026-05-26 22:24:59 -0400

    Init scraping file

---
+++ web_scrape/scrape.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pandas as pd
+import json
+import csv
+
+try:      
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Enable headless mode
+    options.add_argument('--disable-gpu')  # Optional, recommended for Windows
+    options.add_argument('--window-size=1920x1080')  # Optional, set window size
+
+    driver.get("https://owasp.org/Top10/2025/") #original url has changed so I have to use this one
+    title = driver.title # Find the title.
+    print(f"Title: {title}")
+
+    body = driver.find_element(By.CSS_SELECTOR,'body') 
+    
+    
+            
+except Exception as e:
+    print(f"An exception occurred: {type(e).__name__}{e}")
+finally:
+    driver.quit()
