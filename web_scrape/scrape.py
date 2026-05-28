a82c3116e935062c867e26e11bbb203335eb5be4
Brittany Price <bprice0496@gmail.com>
2026-05-28 02:53:35 -0400

    Testing two games

--- web_scrape/scrape.py
+++ web_scrape/scrape.py
@@ -13,13 +13,20 @@ try:
     options.add_argument('--disable-gpu')  # Optional, recommended for Windows
     options.add_argument('--window-size=1920x1080')  # Optional, set window size
 
-    driver.get("https://store.steampowered.com/category/action_tps") #original url has changed so I have to use this one
+    driver.get("https://store.steampowered.com/category/action_tps")
     title = driver.title # Find the title.
     print(f"Title: {title}")
-
+    
     body = driver.find_element(By.CSS_SELECTOR,'body') 
     
+    games = {
+        "https://store.steampowered.com/app/1332010/Stray/",
+        "https://store.steampowered.com/app/553850/HELLDIVERS_2/"
+    }
     
+    for get_game in games:
+        driver.get(get_game)
+        print(driver.title)
             
 except Exception as e:
     print(f"An exception occurred: {type(e).__name__}{e}")
