2739a599787d31f71594e3a5d2c8b3d29088ae3c
Brittany Price <bprice0496@gmail.com>
2026-05-28 21:35:24 -0400

    Added title to dictionary

--- web_scrape/scrape.py
+++ web_scrape/scrape.py
@@ -24,30 +24,50 @@ try:
     wait = WebDriverWait(driver, 15)
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href*='/app/']")))
 
-    title = driver.title # Find the title.
+    title = driver.title 
     print(f"Title: {title}")
     
     body = driver.find_element(By.CSS_SELECTOR,'body') 
+    time.sleep(5)
     games = driver.find_elements(By.CSS_SELECTOR, "a[href*='/app/']")
+    
     #sets --> handle duplicates
     handle_duplicates = set()
     
+    game_dict = []
+    print("Number of game elements found:", len(games))
+    
     for get_game in games:
+        # skip links that don't have an image
+        if not get_game.find_elements(By.TAG_NAME, "img"):
+         continue
+     
+        image = get_game.find_element(By.TAG_NAME,"img")
+        title = image.get_attribute("alt")
+        
+        #handle urls
         url = get_game.get_attribute("href")
         
-        if url:
-            handle_duplicates.add(url)
-        print(url)
-            
-        #Conditional to check for "img" --> extract image and then title from the image
-        # Use find_element for image
-    
+        if not url:
+            continue
         
+        #Normalizing urls
+        url = url.split("?")[0]      # remove ?snr=
+        url = url.rstrip("/")        # remove trailing /
+        
+        if url in handle_duplicates:
+            continue
+        
+        handle_duplicates.add(url)
+
         #build a dictionary for: title, publisher, release date, reviews (for analytics)
-        game_dict = {
-            "Url": url
-        }
-        # print(game_dict)
+        game_dict.append({
+            "Title": title,
+            "Url": url,
+        })
+        
+    game_frame = pd.DataFrame(game_dict)
+    print(game_frame)
         
 except Exception as e:
     print(f"An exception occurred: {type(e).__name__}{e}")
