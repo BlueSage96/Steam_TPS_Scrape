bd417168a1b205be531068550721ad29bbe201df
Brittany Price <bprice0496@gmail.com>
2026-06-06 23:29:05 -0400

    Display original data & changed column names

--- web_scrape/clean.py
+++ web_scrape/clean.py
@@ -1,5 +1,9 @@
 #Pandas clean info
 import pandas as pd
 
-df_tps = pd.read_csv("steam_deck.csv")
-print(df_tps)
+data_tps = pd.read_csv("steam_deck.csv")
+print(f"Original data:\n {data_tps}") #Vanilla data
+
+data_tps = data_tps.rename(columns={"ReviewUrl":"Review URL", "Count":"User Reviews", "Score":"User Score"})
+print(f"Changed column names:\n {data_tps}")
+
