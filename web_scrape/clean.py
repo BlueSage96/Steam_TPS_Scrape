c393ed50e4ade63ffcf0452b7b57fb4c81e5c3cd
Brittany Price <bprice0496@gmail.com>
2026-06-06 23:30:11 -0400

    Changed "User Reviews" column to only have integers

--- web_scrape/clean.py
+++ web_scrape/clean.py
@@ -7,3 +7,10 @@ print(f"Original data:\n {data_tps}") #Vanilla data
 data_tps = data_tps.rename(columns={"ReviewUrl":"Review URL", "Count":"User Reviews", "Score":"User Score"})
 print(f"Changed column names:\n {data_tps}")
 
+data_tps["User Reviews"] = (
+    data_tps["User Reviews"]
+    .astype(str)
+    .str.replace("User Reviews", "", regex=False)
+    .str.strip()
+)
+print(f"Integer for reviews: {data_tps["User Reviews"]}")
