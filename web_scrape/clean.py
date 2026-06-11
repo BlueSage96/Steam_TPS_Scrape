7868318cbcd1908c8b56a5ae2124b15f8195cb07
Brittany Price <bprice0496@gmail.com>
2026-06-11 03:01:58 -0400

    Split "Original Prices" into a series of lists

--- web_scrape/clean.py
+++ web_scrape/clean.py
@@ -4,9 +4,11 @@ import pandas as pd
 data_tps = pd.read_csv("steam_deck.csv")
 print(f"Original data:\n {data_tps}") #Vanilla data
 
-data_tps = data_tps.rename(columns={"ReviewUrl":"Review URL", "Count":"User Reviews", "Score":"User Score"})
+#Rename some columns
+data_tps = data_tps.rename(columns={"ReviewUrl":"Review URL", "Count":"User Reviews", "Score":"User Score","Prices":"Original Prices"})
 print(f"Changed column names:\n {data_tps}")
 
+#Convert user reviews from strings to integers
 data_tps["User Reviews"] = (
     data_tps["User Reviews"]
     .astype(str)
@@ -14,3 +16,8 @@ data_tps["User Reviews"] = (
     .str.strip()
 )
 print(f"Integer for reviews:\n {data_tps}")
+
+#Split "Original Prices" into a Series of lists
+split_prices = data_tps['Original Prices'].str.replace('\n', ' | ')
+split_prices = split_prices.str.split(' | ',regex=False)
+print(split_prices)
