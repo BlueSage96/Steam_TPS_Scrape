2b3b85ced8deb156426ef85368f7c9b97000b846
Brittany Price <bprice0496@gmail.com>
2026-06-13 17:57:24 -0400

    Loaded in clean game csv

--- sql/tps_sql.py
+++ sql/tps_sql.py
@@ -4,7 +4,8 @@ import pandas as pd
 # use conn & sqlite3 to create database
 conn = sqlite3.connect("tps.db")
 #use pd.read_csv
-
+game_tps = pd.read_csv("../web_scrape/clean_games.csv")
+print(game_tps)
 #csv names.to_sql() --> look up params
 
 #print tables to confirm existence --> 
