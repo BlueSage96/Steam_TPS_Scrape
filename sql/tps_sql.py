922d8071704d7a2c234bfb20e6b8ab8a9c3a4b50
Brittany Price <bprice0496@gmail.com>
2026-06-13 17:50:24 -0400

    Created new database

---
+++ sql/tps_sql.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+
+# use conn & sqlite3 to create database
+conn = sqlite3.connect("tps.db")
+#use pd.read_csv
+
+#csv names.to_sql() --> look up params
+
+#print tables to confirm existence --> 
+# variable = pd.read_sql then print(variable)
+
+#close sql database
